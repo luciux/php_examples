5e3e4a22b4ce6541b9fe7cd5f14247dbc55f5580
Luciano <luciano.matiello@gmail.com>
2012-11-09 09:49:21 -0200

    Inclusao do Exercícios.txt

---
+++ php/perfil.php
@@ -0,0 +1,7 @@
+<?php 
+
+	echo"<p> Isso é apenas um teste!";
+	echo"<br/>";
+	echo '<a href="sobre.php">Voltar!</a>';		
+
+?>
--- php/sobre.php
+++ php/sobre.php
@@ -4,5 +4,6 @@
 	
 	cabecalho("Pagina Inicial");
 	echo '<p> Ola Mundo </p>';
+	echo'<a href="perfil.php">Perfil</a>';
 	rodape();
 ?>
